181c839ab55dd9345d68f9ef5e9c1c6783e80fad
NataliaBorowskaPJATK <s25566@pjwstk.edu.pl>
2023-10-12 13:55:09 +0200

    jump
    add jump

--- Assets/BasicPlayerMovement.cs
+++ Assets/BasicPlayerMovement.cs
@@ -6,6 +6,10 @@ public class BasicPlayerMovement : MonoBehaviour
     private Rigidbody2D _rb;
     private float _xInput;
     [SerializeField] private float _speed = 5;
+
+    private bool _performJump;
+    private bool _isGrounded;
+    [SerializeField] private float _jumpForce = 5;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -15,10 +19,31 @@ public class BasicPlayerMovement : MonoBehaviour
    private void Update()
    {
        _xInput = Input.GetAxis("Horizontal");
+
+       if (Input.GetButtonDown("Jump") && _isGrounded)
+       {
+           _performJump = true;
+       }
+       
    }
 
    private void FixedUpdate()
    {
        _rb.velocity = new Vector2(_xInput * _speed, _rb.velocity.y );
+
+       if (_performJump)
+       {
+           _performJump = false;
+           _rb.AddForce(new Vector2(0,_jumpForce), ForceMode2D.Impulse);
+       }
+   }
+
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+       _isGrounded = true;
+   }
+   private void OnCollisionExit2D(Collision2D other)
+   {
+       _isGrounded = false;
    }
 }
