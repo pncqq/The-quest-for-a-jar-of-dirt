de2cdc2764a1844c4727fd42b4fd06ea8262bc28
PiotrNowickii <s24667@pjwstk.edu.pl>
2023-10-14 20:25:42 +0200

    Added gold coins and a coin counter

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinCounter : MonoBehaviour
+{
+    public static CoinCounter instance;
+    public int coinCount;
+    public TMP_Text coinText;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        coinText.text = "Coins: " + coinCount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void IncreaseCoinCount(int val)
+    {
+        coinCount += val;
+        coinText.text = "Coins: " + coinCount.ToString();
+    }
+}
