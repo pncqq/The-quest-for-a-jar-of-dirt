5163c7c563d79cd2485bd42ee3bb7fa5701022b5
Filip Michalski <118136393+pncqq@users.noreply.github.com>
2023-10-15 21:32:55 +0200

    Animacje
    -RawAxis zamiast Axis dla movementu
    -Cleaunp w PlayerMovement.css
    -Animacje Playera

--- Assets/Scripts/BasicPlayerMovement.cs
+++ Assets/Scripts/BasicPlayerMovement.cs
@@ -1,49 +1,100 @@
 using System;
 using UnityEngine;
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class BasicPlayerMovement : MonoBehaviour
 {
+    //Compontents
     private Rigidbody2D _rb;
-    private float _xInput;
-    [SerializeField] private float _speed = 5;
+    private Animator animator;
+    private SpriteRenderer _sprite;
 
+    //Movement
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _jumpForce = 5;
+    private float _xInput;
     private bool _performJump;
     private bool _isGrounded;
-    [SerializeField] private float _jumpForce = 5;
+
+    //Animator
+    private static readonly int XVelocity = Animator.StringToHash("xVelocity");
+    private static readonly int YVelocity = Animator.StringToHash("yVelocity");
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
     //Update is called once per frame
-   private void Update()
-   {
-       _xInput = Input.GetAxis("Horizontal");
+    private void Update()
+    {
+        _xInput = Input.GetAxisRaw("Horizontal");
 
-       if (Input.GetButtonDown("Jump") && _isGrounded)
-       {
-           _performJump = true;
-       }
-       
-   }
+        //Jump check
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _performJump = true;
+        }
+    }
 
-   private void FixedUpdate()
-   {
-       _rb.velocity = new Vector2(_xInput * _speed, _rb.velocity.y );
+    private void FixedUpdate()
+    {
+        //Variables
+        var velocity = new Vector2(_xInput * _speed, _rb.velocity.y);
+        var isMoving = velocity.x != 0;
 
-       if (_performJump)
-       {
-           _performJump = false;
-           _rb.AddForce(new Vector2(0,_jumpForce), ForceMode2D.Impulse);
-       }
-   }
+        //Left-right movement & sprite orientation
+        _rb.velocity = velocity;
+        if (velocity.x != 0)
+        {
+            var isFacingRight = velocity.x > 0 ? 1 : 0;
+            _sprite.flipX = isFacingRight != 1;
+        }
 
-   private void OnCollisionEnter2D(Collision2D other)
-   {
-       _isGrounded = true;
-   }
-   private void OnCollisionExit2D(Collision2D other)
-   {
-       _isGrounded = false;
-   }
+        //Jumping
+        if (_performJump)
+        {
+            _performJump = false;
+            _rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+        }
+
+        //UpdateAnimation
+        UpdateAnimation(velocity, isMoving);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        _isGrounded = true;
+    }
+
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        _isGrounded = false;
+    }
+
+    private void UpdateAnimation(Vector2 velocity, bool isMoving)
+    {
+        //Animator variables
+        animator.SetFloat(XVelocity, velocity.x);
+        animator.SetFloat(YVelocity, velocity.y);
+        animator.SetBool(IsMoving, isMoving);
+        animator.SetBool(IsJumping, !_isGrounded);
+    }
+
+
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     _isGrounded = true;
+    // }
+    //
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     _isGrounded = false;
+    // }
 }
