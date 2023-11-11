6152ae70150881fbe46865f4b14a5de20ee251a2
Filip Michalski <118136393+pncqq@users.noreply.github.com>
2023-11-11 19:48:12 +0100

    Dead anim fixed
    and cleanup

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,6 +6,9 @@ public class HealthSystem : MonoBehaviour
     public static HealthSystem Instance;
     public int maxHealth = 100;
     public int currentHealth;
+    internal bool IsDead;
+    internal bool IsHit;
+
     public HealthBar healthBar;
     [SerializeField] Rigidbody2D _playerRB;
     [SerializeField] private BasicPlayerMovement _playerMovement;
@@ -20,6 +23,10 @@ public class HealthSystem : MonoBehaviour
 
     private void Update()
     {
+        //Check if dead
+        if (currentHealth <= 0)
+            IsDead = true;
+
         //Nie zmniejszaj poniżej 0
         if (currentHealth < 0) currentHealth = 0;
         if (currentHealth > maxHealth) currentHealth = maxHealth;
@@ -29,12 +36,17 @@ public class HealthSystem : MonoBehaviour
 
     public void TakeDamage(int damage, Rigidbody2D enemy)
     {
+        //Update health
         currentHealth -= damage;
+        //Update healthbar
+        healthBar.SetHealth(currentHealth);
+        //Update isHit field
+        IsHit = true;
+
+        //Knockback
         _playerRB.velocity = enemy.transform.position.x > transform.position.x
             ? new Vector2(-1f, 5f)
             : new Vector2(1f, 5f);
-
-        healthBar.SetHealth(currentHealth);
     }
 
     public void Heal(int healSize)
