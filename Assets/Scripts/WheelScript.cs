93f2367a49e863d5dd840c92226cfd7db40fdece
PiotrNowickii <s24667@pjwstk.edu.pl>
2023-11-18 20:00:07 +0100

    Renamed some files, added wheel animation

--- Assets/Scripts/WheelScript.cs
+++ Assets/Scripts/WheelScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,13 @@ using UnityEngine;
 public class WheelScript : MonoBehaviour
 {
     private bool inRange;
+    private Animator _animator;
     [SerializeField] private BoatScript boat;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -13,6 +20,7 @@ public class WheelScript : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                _animator.SetTrigger("IsSpun");
                 boat.isDone = false;
                 boat.isIdle = false;
             }
